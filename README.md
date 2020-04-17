2c183d85da70093d7d3e1155216ac9b6ec104403
Somdeb Chatterjee <somdeb.ch@gmail.com>
2020-04-17 19:02:34 +0530

    Minor changes

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## blast2gff
 
-Convert blastn alignment files to GFFv3 annotation format, for example, after aligning de-novo predicted mRNA transcripts to a reference genome.
+Convert blastn alignment outputs to GFFv3 annotation format, such as after aligning de-novo predicted mRNA transcripts to a reference (prokaryotic) genome.
 In case of strand specific alignment, it can also merge the corresponding GFFs.
 
 ### Usage
--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 // License: MIT
 
 /*
- * Convert blastn alignments to GFFv3 file
+ * Convert strand-specific blastn alignment files to GFFv3 annotation format.
+ *
+ * Currently intended for use with prokaryotic genomes.
+ *
+ * TODO: 
+ *  1) Sort GFF by coordinates.
+ *  2) Handle eukaryotes too.
  */
 
 package main
@@ -16,36 +22,49 @@ import (
 
 const (
 	__reference   = "Chromosome"
-	__method      = "denovo"
+	__method      = "de novo"
 	__feature     = "gene"
 	__description = "de novo predicted transcript"
-	__version     = "0.1-bugfix"
+	__version     = "0.1"
 )
 
+/* Print help text and exit */
+func help() {
+	fmt.Printf("\nUsage:\n\n")
+	fmt.Printf("    Forward strand alignment to GFF:\n")
+	fmt.Printf("	~$ blast2gff convert watson alignment_foward_strand.aln > watson.gff\n\n")
+	fmt.Printf("    Reverse strand alignment to GFF:\n")
+	fmt.Printf("	~$ blast2gff convert crick alignment_reverse_strand.aln > crick.gff\n\n")
+	fmt.Printf("    Merge:\n")
+	fmt.Printf("	~$ blast2gff merge watson.gff crick.gff > transcript_annotation.gff\n\n")
+}
+
+/*
+ * Convert BLASTN alignment output to GFFv3
+ *
+ * TODO: Need to sort the gff by coordinates. Currently unsorted.
+ */
 func convert(strand, alnfile string) {
-	//Parse a BLASTN alignment file and convert it to a GFFv3 file
-	/*
-	 * TODO: Need to include an additional stage where we
-	 * sort the gff by coordinates. Currently it's unsorted.
-	 */
 
 	if strand != "watson" {
 		if strand != "crick" {
-			println("Strand should be either watson or crick")
+			help()
 			os.Exit(0)
 		}
 	}
 
 	fh, err := os.Open(alnfile)
 	if err != nil {
-		fmt.Printf("Could not open file: %s\n", alnfile)
+		fmt.Printf("Could not open alignment file: %s\n", alnfile)
 		os.Exit(0)
 	}
+
 	defer fh.Close()
 	line := bufio.NewScanner(fh)
 	id_count, b5_count, b3_coord, b5_coord := 0, 0, 0, 0
 	var name string
 	var split_line []string
+
 	for line.Scan() {
 
 		split_line = strings.Split(line.Text(), " ")
@@ -63,9 +82,7 @@ func convert(strand, alnfile string) {
 			if b5_count == 0 {
 				b5_coord, _ = strconv.Atoi(split_line[2])
 			}
-			b5_count += 1//this updates everytime
-			// this updates everytime, we just need the last
-			// value
+			b5_count += 1
 			b3_coord, _ = strconv.Atoi(split_line[6])
 		}
 
@@ -91,11 +108,11 @@ func convert(strand, alnfile string) {
 	}
 }
 
+/* Read files concurrently */
 func scanfile(file string, queue chan string) {
-	// scanfile allows reading two files concurrently
 	fh, err := os.Open(file)
 	if err != nil {
-		fmt.Printf("Could not open file: %s\n", file)
+		fmt.Printf("Could not open GFF file: %s\n", file)
 		os.Exit(0)
 	}
 	defer fh.Close()
@@ -106,17 +123,18 @@ func scanfile(file string, queue chan string) {
 	close(queue)
 }
 
+/* Merge two GFF files */
 func merge(fwdfile, revfile string) {
-	// Merge two GFF files
+
 	fmt.Printf("##gff-version 3\n")
-	fmt.Printf("#!build blast2gff/denovo\n")
+	fmt.Printf("#!build blast2gff/de_novo\n")
 
 	queue := make(chan string)
 	go scanfile(revfile, queue)
 
 	fh, err := os.Open(fwdfile)
 	if err != nil {
-		fmt.Printf("Could not open file: %s", fwdfile)
+		fmt.Printf("Could not open GFF file: %s", fwdfile)
 		os.Exit(0)
 	}
 	defer fh.Close()
@@ -144,17 +162,23 @@ func merge(fwdfile, revfile string) {
 func main() {
 
 	if len(os.Args) == 1 {
-		println("No commands given")
+		println("No commands given. Try 'blast2gff help'")
 		os.Exit(0)
 	}
 
 	if os.Args[1] == "version" {
-		fmt.Printf("blast2gff, built on: %s\n", __version)
+		fmt.Printf("blast2gff v%s\n", __version)
+		os.Exit(0)
+	}
+
+	if os.Args[1] == "help" {
+		fmt.Printf("blast2gff v%s\n", __version)
+		help()
 		os.Exit(0)
 	}
 
 	if len(os.Args) != 4 {
-		println("Invalid arguments")
+		println("Whoops! Try 'blast2gff help'")
 		os.Exit(0)
 	}
 
@@ -167,7 +191,7 @@ func main() {
 		merge(os.Args[2], os.Args[3])
 
 	default:
-		println("Unrecognized command")
+		println("Whoops! Try 'blast2gff help'")
 
 	}
 }
